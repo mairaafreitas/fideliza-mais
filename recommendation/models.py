ae3a4d72ac4ed01c37460714361cd7cb5ad503d2
Maíra Freitas <maira.freitas@maistodos.com.br>
2021-12-21 08:45:22 -0300

    feat: add verbose name to models

--- recommendation/models.py
+++ recommendation/models.py
@@ -4,10 +4,18 @@ from user.models import User
 
 
 class Recommendation(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recommended_email = models.EmailField(unique=True, null=False, blank=False)
-    has_accepted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(
+        auto_now_add=True, editable=False, verbose_name="Data de criação"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Usuário Indicador"
+    )
+    recommended_email = models.EmailField(
+        unique=True, null=False, blank=False, verbose_name="Email do Indicado"
+    )
+    has_accepted = models.BooleanField(
+        default=False, verbose_name="Aceitou a indicação?"
+    )
 
     def __str__(self):
         return self.recommended_email
--- user/models.py
+++ user/models.py
@@ -4,14 +4,23 @@ from user.validators import cpf_validator
 
 
 class User(models.Model):
-    name = models.CharField(max_length=80, null=False, blank=False)
+    name = models.CharField(max_length=80, null=False, blank=False, verbose_name="Nome")
     document = models.CharField(
-        max_length=11, unique=True, null=False, blank=False, validators=[cpf_validator]
+        max_length=11,
+        unique=True,
+        null=False,
+        blank=False,
+        validators=[cpf_validator],
+        verbose_name="CPF",
     )
-    email = models.EmailField(unique=True, null=False, blank=False)
-    birth_date = models.DateField()
-    phone = models.CharField(max_length=11, unique=True, null=False, blank=False)
-    balance = models.IntegerField()
+    email = models.EmailField(
+        unique=True, null=False, blank=False, verbose_name="Email"
+    )
+    birth_date = models.DateField(verbose_name="Data de Nascimento")
+    phone = models.CharField(
+        max_length=11, unique=True, null=False, blank=False, verbose_name="Celular"
+    )
+    balance = models.IntegerField(verbose_name="Pontuação", default=0)
 
     def __str__(self):
         return self.name
